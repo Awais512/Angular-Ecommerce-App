28b2e840677e29cd6205983785f28deb4a19c115
awais512 <raza.awais25@gmail.com>
2021-07-07 21:57:45 +0500

    Creating style files for applications

--- apps/ngshop/src/app/pages/product-list/product-list.component.ts
+++ apps/ngshop/src/app/pages/product-list/product-list.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'bluebit-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
