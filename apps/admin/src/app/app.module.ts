bc9bf158e3494537eb8a54f748bd9c4535deb42d
awais512 <raza.awais25@gmail.com>
2021-07-08 18:40:59 +0500

    Creating Categories List Table

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { Table, TableModule } from 'primeng/table';
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ShellComponent } from './shared/shell/shell.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+
+const UX_MODULE = [CardModule, ToolbarModule, ButtonModule, TableModule];
 
 const routes: Routes = [
   {
@@ -15,6 +22,10 @@ const routes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent,
+      },
+      {
+        path: 'categories',
+        component: CategoriesListComponent,
       },
     ],
   },
@@ -26,10 +37,12 @@ const routes: Routes = [
     ShellComponent,
     DashboardComponent,
     SidebarComponent,
+    CategoriesListComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+    ...UX_MODULE,
   ],
   providers: [],
   bootstrap: [AppComponent],
