bf76e8f03279f900ad2bd05249d2f2ad3398519b
awais512 <raza.awais25@gmail.com>
2021-07-08 13:36:17 +0500

    Admin Navigation Panel

--- apps/admin/src/app/shared/sidebar/sidebar.component.ts
+++ apps/admin/src/app/shared/sidebar/sidebar.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'admin-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
   constructor() {}
