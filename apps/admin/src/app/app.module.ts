821a8d3bcf6cb3d3ce5e190977c67e8582f09489
awais512 <raza.awais25@gmail.com>
2021-07-08 11:21:21 +0500

    Creating the main shell of application

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { ShellComponent } from './shared/shell/shell.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ShellComponent, DashboardComponent, SidebarComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
